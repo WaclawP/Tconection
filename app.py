38ed7e6ea5210d6c726902d208ec3895cb997fd6
waclaw_pawel <waclaw.pawel@wp.pl>
2025-03-28 18:48:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ if 'polling_interval' not in st.session_state:
     st.session_state['polling_interval'] = None
 if 'access_token' not in st.session_state:
     st.session_state['access_token'] = None
-konto = st.text_input("wpisz nazwę konta")
+
 def send_token_via_webhook(token):
     try:
-        headers = {'Content-Type': 'application/json', 'konto': konto}
+        headers = {'Content-Type': 'application/json'}
         data = {'access_token': token}
         response = requests.post(WEBHOOK_URL, headers=headers, data=json.dumps(data))
         response.raise_for_status()
@@ -49,29 +49,31 @@ def get_token():
         get_token()  # ponowne wywołanie funkcji
     else:
         st.error(f"Błąd podczas pobierania tokenu: {response.json()}")
-    if st.button("Authorize with Allegro") and st.session_state['device_code'] is None:
-        auth_string = base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode()).decode()
-        device_url = "https://allegro.pl/auth/oauth/device"
-        headers = {
-            "Authorization": f"Basic {auth_string}",
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-        data = {
-            "client_id": CLIENT_ID
-        }
-        device_response = requests.post(device_url, headers=headers, data=data)
-        if device_response.status_code == 200:
-            data = device_response.json()
-            user_code = data.get('user_code')
-            verification_uri_complete = data.get('verification_uri_complete')
-            st.session_state['device_code'] = data.get('device_code')
-            st.session_state['polling_interval'] = data.get('interval')
-            st.markdown(f"Proszę odwiedzić: {verification_uri_complete}  ") #kod: {user_code} mozna dodac ale czesto jest
-            while st.session_state['access_token'] is None and st.session_state['device_code'] is not None:
-                time.sleep(3)
-                get_token()
-        else:
-            st.error("Nie udało się zainicjować autoryzacji.")
-    
-    if st.session_state['access_token']:
-        st.success(f"=================================================")
+
+if st.button("Authorize with Allegro") and st.session_state['device_code'] is None:
+    auth_string = base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode()).decode()
+    device_url = "https://allegro.pl/auth/oauth/device"
+    headers = {
+        "Authorization": f"Basic {auth_string}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "client_id": CLIENT_ID
+    }
+    device_response = requests.post(device_url, headers=headers, data=data)
+    if device_response.status_code == 200:
+        data = device_response.json()
+        user_code = data.get('user_code')
+        verification_uri_complete = data.get('verification_uri_complete')
+        st.session_state['device_code'] = data.get('device_code')
+        st.session_state['polling_interval'] = data.get('interval')
+        konto = st.text_input("wpisz nazwę konta")
+        st.markdown(f"Proszę odwiedzić: {verification_uri_complete}  ") #kod: {user_code} mozna dodac ale czesto jest
+        while st.session_state['access_token'] is None and st.session_state['device_code'] is not None:
+            time.sleep(3)
+            get_token()
+    else:
+        st.error("Nie udało się zainicjować autoryzacji.")
+
+if st.session_state['access_token']:
+    st.success(f"=================================================")
